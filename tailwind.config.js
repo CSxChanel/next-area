d9adb2f61cf2282040f041a3884a2fe8cb53b0e0
cp <120399183+CSxChanel@users.noreply.github.com>
2024-07-19 17:08:07 +0000

    perubahan global.css dan tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,25 +6,26 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-        extend: {},
-        fontFamily: {
-            body: "Poppins"
-        },
-        container: {
-            center: true,
-            padding: {
-                DEFAULT: "15px",
-                md: "25px",
-                lg: "20px"
-            }
-        },
-
-        screens: {
-            sm: "640px",
-            md: "768px",
-            lg: "960px",
-            xl: "1200px"
-        }
+    extend: {
+      fontFamily: {
+        body: ["Poppins", "sans-serif"],
+      },
     },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "15px",
+        md: "25px",
+        lg: "20px",
+      },
+    },
+
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "960px",
+      xl: "1200px",
+    },
+  },
   plugins: [],
 };
