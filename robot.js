117434b5b953bbe2e0bea044a8c610611d72b85f
CSxChanel <csudrajat0@gmail.com>
2024-08-06 09:17:21 +0700

    uplod

---
+++ robot.js
@@ -0,0 +1,9 @@
+export default function robots() {
+   return {
+      rules: {
+         userAgent: '*',
+         allow: '/',
+      },
+      sitemap: 'https://indihome-area.com/sitemap.xml',
+   };
+}
