02c332c690cc57db00b6bf96c9c9253cb0c7f055
CSxChanel <csudrajat80@gmail.com>
2024-08-11 20:51:59 +0700

    convers images

--- src/components/BannerSlider/BannerSlider.jsx
+++ src/components/BannerSlider/BannerSlider.jsx
@@ -5,20 +5,20 @@ import "slick-carousel/slick/slick-theme.css";
 // Sesuaikan path sesuai dengan struktur proyek Anda
 import Image from "next/image";
 // Import gambar
-import Baner1 from "@/public/assets/baner/14-IndihomeTV-720x400px_M.png";
-import Baner2 from "@/public/assets/baner/720-x-400px0_M.jpg";
-import Baner3 from "@/public/assets/baner/Banner_Homepage_Mobile_720x400_M.jpg";
-import Baner4 from "@/public/assets/baner/banner_Lapor_Gangguan_720x400_M.jpg";
-import Baner5 from "@/public/assets/baner/banner_selfcare_720x400_M.jpg";
-import Baner6 from "@/public/assets/baner/banner_tsel_m_M.jpg";
-import Baner7 from "@/public/assets/baner/gamer720-x-400_1_M.jpg";
-import Baner8 from "@/public/assets/baner/Indihome_PL_Homepage_Mobile_720x400_M.jpg";
-import Baner9 from "@/public/assets/baner/Mobile_Homepage_720x400-_MAY_M.jpg";
-import Baner10 from "@/public/assets/baner/Mobile-(720x400)-NETFLIX-2024-(rev5)_M.jpg";
-import Baner11 from "@/public/assets/baner/Movie_LPC_HomepageMobile720x400_M.jpg";
-import Baner12 from "@/public/assets/baner/Paket-IndiHome-Netflix-720-x-400_M.jpg";
-import Baner13 from "@/public/assets/baner/Pay_in_Advance_New_720x400_M.png";
-import Baner14 from "@/public/assets/baner/Tsel-One-Paket-Dynamic-Feb-240201-Banner-Indihome-720x400_M.jpg";
+import Baner1 from "@/public/banners/14-IndihomeTV-720x400px_M.png";
+import Baner2 from "@/public/banners/720-x-400px0_M.jpg";
+import Baner3 from "@/public/banners/Banner_Homepage_Mobile_720x400_M.jpg";
+import Baner4 from "@/public/banners/banner_Lapor_Gangguan_720x400_M.jpg";
+import Baner5 from "@/public/banners/banner_selfcare_720x400_M.jpg";
+import Baner6 from "@/public/banners/banner_tsel_m_M.jpg";
+import Baner7 from "@/public/banners/gamer720-x-400_1_M.jpg";
+import Baner8 from "@/public/banners/Indihome_PL_Homepage_Mobile_720x400_M.jpg";
+import Baner9 from "@/public/banners/Mobile_Homepage_720x400-_MAY_M.jpg";
+import Baner10 from "@/public/banners/Mobile-(720x400)-NETFLIX-2024-(rev5)_M.jpg";
+import Baner11 from "@/public/banners/Movie_LPC_HomepageMobile720x400_M.jpg";
+import Baner12 from "@/public/banners/Paket-IndiHome-Netflix-720-x-400_M.jpg";
+import Baner13 from "@/public/banners/Pay_in_Advance_New_720x400_M.png";
+import Baner14 from "@/public/banners/Tsel-One-Paket-Dynamic-Feb-240201-Banner-Indihome-720x400_M.jpg";
 
 import { CustomNextArrow, CustomPrevArrow } from "./CustomArrow"; // Sesuaikan path sesuai dengan struktur proyek Anda
 
