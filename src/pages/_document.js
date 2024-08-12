68fb3e2a7a8c362668f4fcbe2c9546cdb80f3f1a
CSxChanel <csudrajat80@gmail.com>
2024-08-13 05:12:00 +0700

    mmerubah tampilan sliderImage

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="id_ID" className="scroll-smooth">
+    <Html lang="id_ID">
       <Head>
         <link />
         <link
