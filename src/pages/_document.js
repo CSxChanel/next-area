07708e3a98e9d41e680a73d70dcac900a5e2c560
CSxChanel <120399183+CSxChanel@users.noreply.github.com>
2024-08-16 00:34:25 +0000

    _documen.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,19 +1,20 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { Poppins } from "@next/font/google";
+
+const poppins = Poppins({
+  weight: ["400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export default function Document() {
-    return (
-        <Html lang="id">
-            <Head>
-                <link />
-                <link
-                    href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800;900&display=swap"
-                    rel="stylesheet"
-                />
-            </Head>
-            <body>
-                <Main />
-                <NextScript />
-            </body>
-        </Html>
-    );
+  return (
+    <Html lang="id-ID">
+      <Head />
+      <body className={poppins.className}>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
