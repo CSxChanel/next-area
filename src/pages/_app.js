e5b14ca674c6be8e64a0c22ab8aff82ee8e230e6
CSxChanel <120399183+CSxChanel@users.noreply.github.com>
2024-08-16 00:53:28 +0000

    fon Poppins

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,16 @@
 import Layout from "@/components/Layout";
 import "@/styles/globals.css"; // Pastikan untuk mengimpor CSS global
+import { Poppins } from "@next/font/google";
+
+const poppins = Poppins({
+  weight: ["400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+  display: "swap",
+});
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
+    <Layout clasName={poppins.className}>
       <Component {...pageProps} />
     </Layout>
   );
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,17 +1,10 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import { Poppins } from "@next/font/google";
-
-const poppins = Poppins({
-  weight: ["400", "500", "600", "700", "800", "900"],
-  subsets: ["latin"],
-  display: "swap",
-});
 
 export default function Document() {
   return (
     <Html lang="id-ID">
       <Head />
-      <body className={poppins.className}>
+      <body >
         <Main />
         <NextScript />
       </body>
